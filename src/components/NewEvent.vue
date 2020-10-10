7c8edd0b0d69b894e0a72787bda7cd420977e3b5
andreasvogt89 <andreas.vogt89@bluewin.ch>
2020-10-10 19:57:27 +0200

    working on Event.vue

--- src/components/NewEvent.vue
+++ src/components/NewEvent.vue
@@ -7,49 +7,37 @@
   </div>
   <div class="input">
   <label>
-    <input  type="date" name="username" v-model="input.event.date" placeholder="Datum..">
+    <input  type="date" name="date" v-model="input.event.date" placeholder="Datum..">
   </label>
   </div>
-  <div class="input">
-  <vue-google-autocomplete
-      ref="address"
-      id="map"
-      classname="form-control"
-      placeholder="Please type your address"
-      v-on:placechanged="getAddressData"
-      country="ch"
-  >
-  </vue-google-autocomplete>
+    <div  class="input">
+      <label>
+        <input type="text" name="place" v-model="input.event.place" placeholder="Wo..">
+      </label>
+    </div>
+  <div>
+    <button>
+      Abbrechen
+    </button>
+    <button>
+      Senden
+    </button>
   </div>
 </form>
 </template>
 
 <script>
 import {reactive} from "vue";
-import VueGoogleAutocomplete from 'vue-google-autocomplete'
 
 export default {
   name: "NewEvent",
-  components: { VueGoogleAutocomplete },
   setup() {
     const input = reactive({
       event: {},
-      address: '',
       error: false,
     });
     return { input };
   },
-  mounted() {
-    this.$refs.input.address.focus();
-  },
-
-  methods:{
-    getAddressData: function (addressData, placeResultData, id) {
-      this.input.address = addressData;
-      console.log(placeResultData);
-      console.log(id);
-    }
-  }
 }
 
 </script>
@@ -59,7 +47,8 @@ export default {
   padding: 20px;
 }
 input[type=text] {
-  box-sizing: border-box;
+  height: 1em;
+  width: 10em;
   border: 2px solid #D12662;
   border-radius: 4px;
   font-size: 22px;
@@ -67,7 +56,8 @@ input[type=text] {
   padding: 12px 20px 12px 40px;
 }
 input[type=date] {
-  box-sizing: border-box;
+  height: 1em;
+  width: 10em;
   border: 2px solid #D12662;
   border-radius: 4px;
   font-size: 22px;
---
+++ src/components/newPerson.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="input">
+    <vue-google-autocomplete
+        ref="address"
+        id="map"
+        classname="form-control"
+        placeholder="Please type your address"
+        v-on:placechanged="getAddressData"
+        country="ch"
+    >
+    </vue-google-autocomplete>
+  </div>
+</template>
+
+<script>
+import VueGoogleAutocomplete from "vue-google-autocomplete";
+import {reactive} from "vue";
+
+export default {
+  name: "newPerson",
+  components: { VueGoogleAutocomplete },
+  setup() {
+    const input = reactive({
+      person: {},
+      error: false,
+    });
+    return { input };
+  },
+  mounted() {
+    this.$refs.input.person.address.focus();
+  },
+
+  methods:{
+    getAddressData: function (addressData, placeResultData, id) {
+      this.input.person.address = addressData;
+      console.log(placeResultData);
+      console.log(id);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.input{
+  padding: 20px;
+}
+input[type=text] {
+  height: 1em;
+  width: 10em;
+  border: 2px solid #D12662;
+  border-radius: 4px;
+  font-size: 22px;
+  background-color: #ffffff;
+  padding: 12px 20px 12px 40px;
+}
+input[type=date] {
+  height: 1em;
+  width: 10em;
+  border: 2px solid #D12662;
+  border-radius: 4px;
+  font-size: 22px;
+  background-color: #ffffff;
+  padding: 12px 20px 12px 40px;
+}
+</style>
